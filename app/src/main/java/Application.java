9d71f8f0cad937f1430ec845efb550b7be6b29b7
Mokhathi <Mokhathi@theideaportal.co.za>
2021-09-28 22:36:08 +0200

    Button added to Register and Application class is added

---
+++ app/src/main/java/Application.java
@@ -0,0 +1,4 @@
+public class Application extends android.app.Application
+{
+
+}
--- app/src/main/java/com/example/waterleakage/Register.java
+++ app/src/main/java/com/example/waterleakage/Register.java
@@ -3,12 +3,34 @@ package com.example.waterleakage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register extends AppCompatActivity {
+
+    TextView tvWelcome;
+    TextInputLayout lytName,lytEmail,lytCell;
+    TextInputEditText edtName,edtEmail,edtCell;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        tvWelcome = findViewById(R.id.tvWelcome);
+        lytName = findViewById(R.id.lytName);
+        lytEmail = findViewById(R.id.lytEmail);
+        lytCell = findViewById(R.id.lytCell);
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtCell = findViewById(R.id.edtCell);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+
+
     }
 }
